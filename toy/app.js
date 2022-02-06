c7b5e57af6ea20fd012d10ee24ddbf542e06a3b8
Geuni620 <dlrmsgnl620@naver.com>
2022-02-07 00:58:32 +0900

    todolist review

--- toy/app.js
+++ toy/app.js
@@ -1,29 +1,32 @@
-/*5
-5 50 50 70 80 100
-7 100 95 90 80 70 60 50
-3 70 90 80
-3 70 90 81
-9 100 99 98 97 96 95 94 93 1
-*/
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
 
-const fs = require(`fs`);
-let input = fs.readFileSync(`./input.txt`).toString();
-input = input.split("\n");
-
-const inputC = Number(input[0]); //5
-const inputTestCase = [];
-
-for (let i = 1; i <= inputC; i++) {
-  const arr = input[i].split(` `);
-  const newArr = []; // 5 50 50 70 80 100
-  for (i = 1; i <= arr[0]; i++) {
-    newArr.push(Number(arr[i])); // 50 50 70 80 100
-  }
-  const testCase = {
-    N: arr[0],
-    arr: newArr,
-  };
-  inputTestCase.push(testCase);
+function deleteToDo(event) {
+  const li = event.target.parentElement;
+  li.remove();
 }
 
-console.log(inputTestCase);
+function paintTodo(newTodo) {
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  const button = document.createElement("button");
+
+  button.innerText = "X";
+
+  toDoList.addEventListener("click", deleteToDo);
+
+  li.appendChild(span);
+  li.appendChild(button);
+  span.innerText = newTodo;
+  toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+  event.preventDefault();
+  const newTodo = toDoInput.value;
+  toDoInput.value = "";
+  paintTodo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
