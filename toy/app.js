819fb72686f884bfda3ecc03fb60ec10b9f04ada
dlrmsgnl620 <dlrmsgnl620@naver.com>
2022-02-13 00:32:42 +0900

    todolist review done

--- toy/app.js
+++ toy/app.js
@@ -2,32 +2,33 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
-let toDos = [];
 const TODOS_KEY = "toDos";
+let toDos = [];
 
-function saveToDos() {
-  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
-}
-
-function deleteToDo(event) {
+function deleToDo(event) {
   const li = event.target.parentElement;
-  toDos = toDos.filter((item) => item.id != parseInt(li.id));
-  li.remove();
+  toDos = toDos.filter((toDo) => toDo.id !== parseInt(li.id));
   saveToDos();
+  li.remove();
 }
 
 function paintTodo(newTodo) {
   const li = document.createElement("li");
   li.id = newTodo.id;
   const span = document.createElement("span");
-  span.innerText = newTodo.text;
-
   const button = document.createElement("button");
   button.innerText = "X";
-  button.addEventListener("click", deleteToDo);
+  span.innerText = newTodo.text;
+
   li.appendChild(span);
   li.appendChild(button);
   toDoList.appendChild(li);
+
+  button.addEventListener("click", deleToDo);
+}
+
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function handleToDoSubmit(event) {
