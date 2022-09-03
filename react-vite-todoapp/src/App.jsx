4ddb7cc9e3707727a317a3042505231e20208e77
Geuni <dlrmsgnl620@naver.com>
2022-09-03 17:09:48 +0900

    Add: front completion

--- react-vite-todoapp/src/App.jsx
+++ react-vite-todoapp/src/App.jsx
@@ -1,34 +1,115 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {useState} from "react";
+import "./App.css";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
-function App() {
-  const [count, setCount] = useState(0)
+let todoItemId = 0;
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  const onSubmit = (e) => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
+  return (
+    <div>
+      <TextField
+        id="todo-item-input"
+        label="할일 목록"
+        variant="outlined"
+        onChange={(e) => setInput(e.target.value)}
+        value={input}
+      />
+      <Button variant="outlined" onClick={onSubmit}>
+        Submit
+      </Button>
+    </div>
+  );
+};
+
+const TodoItem = ({todoItem, onTodoItemClick, onRemoveClick}) => {
+  const style = todoItem.isFinished ? {textDecoration: "line-through"} : {};
+
+  return (
+    <li>
+      <span style={style} onClick={() => onTodoItemClick(todoItem)}>
+        {todoItem.todoItemContent}
+      </span>
+      <Button variant="outlined" onClick={() => onRemoveClick(todoItem)}>
+        Remove
+      </Button>
+    </li>
+  );
+};
+
+const TodoItemList = ({todoItemList, onTodoItemClick, onRemoveClick}) => {
+  const todoList = todoItemList.map((todoItem, idx) => {
+    return (
+      <TodoItem
+        key={idx}
+        todoItem={todoItem}
+        onTodoItemClick={onTodoItemClick}
+        onRemoveClick={onRemoveClick}
+      />
+    );
+  });
+
+  return (
+    <div>
+      <ul>{todoList}</ul>
+    </div>
+  );
+};
+
+const App = () => {
+  const [todoItemList, setTodoItemList] = useState([]);
+
+  const onSubmit = (newTodoItem) => {
+    setTodoItemList([
+      ...todoItemList,
+      {
+        id: todoItemId++,
+        todoItemContent: newTodoItem,
+        isFinished: false,
+      },
+    ]);
+  };
+
+  const onTodoItemClick = (clickedTodoItem) => {
+    setTodoItemList(
+      todoItemList.map((todoItem) => {
+        if (clickedTodoItem.id === todoItem.id) {
+          return {
+            id: clickedTodoItem.id,
+            todoItemContent: clickedTodoItem.todoItemContent,
+            isFinished: !clickedTodoItem.isFinished,
+          };
+        } else {
+          return todoItem;
+        }
+      })
+    );
+  };
+
+  const onRemoveClick = (removedTodoItem) => {
+    setTodoItemList(
+      todoItemList.filter((todoItem) => {
+        return todoItem.id !== removedTodoItem.id;
+      })
+    );
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <TodoItemInputField onSubmit={onSubmit} />
+      <TodoItemList
+        todoItemList={todoItemList}
+        onTodoItemClick={onTodoItemClick}
+        onRemoveClick={onRemoveClick}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
