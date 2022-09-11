7b91bff6b2e83cc5eb9931dbb0782436d8ecdccd
Geuni <dlrmsgnl620@naver.com>
2022-09-11 22:31:55 +0900

    Add: useMemo + React.memo

--- performance-optimization/src/Test3.js
+++ performance-optimization/src/Test3.js
@@ -1,15 +1,16 @@
-import React, {useState, memo} from "react";
+import React, {useState, memo, useMemo} from "react";
 
 const App = () => {
   const [parentAge, setParentAge] = useState(0);
-  const [childAge, setChildAge] = useState(0);
+  const name = useMemo(() => {
+    return {
+      lastName: "홍",
+      firstName: "길동",
+    };
+  }, []);
 
   const incrementParentAge = () => {
     setParentAge(parentAge + 1);
-  };
-
-  const incrementChildAge = () => {
-    setChildAge(childAge + 1);
   };
 
   console.log("부모 컴포넌트가 렌더링 되었습니다.");
@@ -19,22 +20,22 @@ const App = () => {
       <h1>부모</h1>
       <p>age: {parentAge}</p>
       <button onClick={incrementParentAge}>부모 나이 증가</button>
-      <button onClick={incrementChildAge}>자녀 나이 증가</button>
-      <Child name={"홍길동"} age={childAge} />
+
+      <Child name={name} />
     </div>
   );
 };
 
 export default App;
 
-const Child = memo(({name, age}) => {
+const Child = memo(({name}) => {
   console.log("자녀도 렌더링 되었어요.");
   return (
     <div>
       <div style={{border: "2px solid powderblue", padding: "10px"}}></div>
       <h3>자녀</h3>
-      <p>name: {name}</p>
-      <p>age: {age}살</p>
+      <p>성: {name.lastName}</p>
+      <p>이름: {name.firstName}</p>
     </div>
   );
 });
