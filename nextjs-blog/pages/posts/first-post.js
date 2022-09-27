fce397c5af70127414a4ea605e59adb276bc769b
이근휘 <dlrmsgnl620@naver.com>
2022-09-27 14:01:53 +0900

    Assets, Metadata, and CSS

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,12 +1,24 @@
 import Link from "next/link";
+import Image from "next/Image";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <h1>First Post</h1>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
+      {/* <img src="/images/profile.jpg" alt="Your Name" /> */}
+      <Image
+        src="/images/profile.jpg" // Route of the image file
+        height={250} // Desired size with correct aspect ratio
+        width={250} // Desired size with correct aspect ratio
+        alt="profileImg"
+      />
     </>
   );
 }
