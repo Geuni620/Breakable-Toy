11c005a1ff80b700e0878e17cd89116b57c83999
Geuni <dlrmsgnl620@naver.com>
2022-09-28 01:17:18 +0900

    Add: Pre-rendering and Data Fetching

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,24 +1,34 @@
 import Link from "next/link";
 import Image from "next/Image";
 import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
       <Head>
-        <h1>First Post</h1>
+        <title>First Post</title>
       </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
+
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
       {/* <img src="/images/profile.jpg" alt="Your Name" /> */}
-      <Image
+      {/* <Image
         src="/images/profile.jpg" // Route of the image file
         height={250} // Desired size with correct aspect ratio
         width={250} // Desired size with correct aspect ratio
         alt="profileImg"
-      />
-    </>
+      /> */}
+    </Layout>
   );
 }
