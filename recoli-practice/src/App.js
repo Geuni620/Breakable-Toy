c53e10793a9e133c7b1a4bc3e30f0aa90901498f
Geuni <dlrmsgnl620@naver.com>
2022-11-02 01:39:49 +0900

    Add: initial setting recoil

---
+++ recoli-practice/src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from "recoil";
+
+export default function App() {
+  const textState = atom({
+    key: "textState", // unique ID (with respect to other atoms/selectors)
+    default: "", // default value (aka initial value)
+  });
+
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
+}
+
+function CharacterCounter() {
+  return (
+    <div>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  const onChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: "charCountState", // unique ID (with respect to other atoms/selectors)
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
