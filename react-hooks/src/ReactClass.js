a6f19ce2c6d48a14579d58f52e7eb471ee28ffcb
Geuni <dlrmsgnl620@naver.com>
2023-03-01 23:50:42 +0900

    Add: class component

--- react-hooks/src/ReactClass.js
+++ react-hooks/src/ReactClass.js
@@ -1,7 +1,7 @@
-import {Component} from "react";
+import {Component, useReducer} from "react";
 
 export function ReactClass() {
-  return <Modal />;
+  return <Counter />;
 }
 
 class Modal extends Component {
@@ -26,6 +26,79 @@ class Modal extends Component {
         >
           버튼
         </button>
+      </div>
+    );
+  }
+}
+
+/*
+function Hello({color, name, isSpecial}) {
+  return (
+    <div style={{color}}>
+      {isSpecial && <b>*</b>}
+      안녕하세요 {name}
+    </div>
+  );
+}
+
+Hello.defaultProps = {
+  name: "이름없음",
+};
+
+class Hello extends Component {
+  static defaultProps = {
+    name: "이름없음",
+  };
+
+  render() {
+    const {color, name, isSpecial} = this.props;
+    return <div style={{color}}></div>;
+  }
+}
+*/
+
+class Counter extends Component {
+  state = {
+    counter: 0,
+    fixed: 1,
+  };
+  handleIncrease = () => {
+    this.setState(
+      {
+        counter: this.state.counter + 1,
+      },
+      () => {
+        console.log(this.state.counter);
+      }
+    );
+  };
+
+  // handleDecrease = () => {
+  //   this.setState(
+  //     {
+  //       counter: this.state.counter - 1,
+  //     },
+  //     () => {
+  //       console.log(this.state.counter);
+  //     }
+  //   );
+  // };
+
+  handleDecrease = () => {
+    this.setState((state) => ({
+      counter: state.counter - 1,
+    }));
+
+    console.log(this.state.counter);
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+        <p>고정된 값: {this.state.fixed}</p>
       </div>
     );
   }
